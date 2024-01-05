f2b0f7e2d5428e776262d40b142312476d853efa
Vasn <vasilisngjunquan@gmail.com>
2024-01-06 05:11:33 +0800

    update error condition

--- 04-linkedlist/get/EXERCISE-LL-Get.py
+++ 04-linkedlist/get/EXERCISE-LL-Get.py
@@ -67,7 +67,7 @@ class LinkedList:
         return temp
 
     def get(self, index):
-        if index < 0 and index >= self.length:
+        if index < 0 or index >= self.length:
             return None
         
         temp = self.head
